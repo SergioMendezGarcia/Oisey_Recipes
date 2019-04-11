e79dcb83d46e3d8dae466fda8f4f0c0949e0f58f
Guille Fernández <guille.fermar94@gmail.com>
2019-04-11 16:43:09 +0200

    cambios aceptados

--- OiseyApp/src/app/app.module.ts
+++ OiseyApp/src/app/app.module.ts
@@ -6,24 +6,17 @@ import { AppComponent } from './app.component';
 import { PieComponent } from './pie/pie.component';
 import { PantallaChefComponent } from './pantalla-chef/pantalla-chef.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
-<<<<<<< HEAD
-=======
 import { ComentariosChefComponent } from './pantalla-chef/comentarios-chef/comentarios-chef.component';
 
->>>>>>> be9f41efe398225b1fa92a774f296a1bc7c9293f
 
 @NgModule({
   declarations: [
     AppComponent,
     PieComponent,
     PantallaChefComponent,
-<<<<<<< HEAD
-    CabeceraComponent
-=======
     CabeceraComponent,
     ComentariosChefComponent
     
->>>>>>> be9f41efe398225b1fa92a774f296a1bc7c9293f
   ],
   imports: [
     BrowserModule,
