c61d6f490c6ff06fc6d20bf2c696cad753bb4333
JosePedro <alejandro.manzanares.01@gmail.com>
2019-04-11 16:04:20 +0200

    routing

--- OiseyApp/src/app/app-routing.module.ts
+++ OiseyApp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PantallaChefComponent } from './pantalla-chef/pantalla-chef.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'chefs/:cnombre', component: PantallaChefComponent, pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
