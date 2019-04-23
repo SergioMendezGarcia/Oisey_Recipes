1aba012d347f2f58e47578d6285bc447019cf672
Guille Fernández <guille.fermar94@gmail.com>
2019-04-23 09:19:02 +0200

    login

--- OiseyApp/src/app/app.module.ts
+++ OiseyApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MapaComponent } from './pedir-recetas/mapa/mapa.component';
 import { ComentariosChefComponent } from './pantalla-chef/comentarios-chef/comentarios-chef.component';
 import { RatingComponent } from './pantalla-chef/rating/rating.component';
 import { ValorarcomentarComponent } from './pantalla-chef/valorarcomentar/valorarcomentar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { ValorarcomentarComponent } from './pantalla-chef/valorarcomentar/valora
     CabeceraComponent,
     ComentariosChefComponent,
     RatingComponent,
-    ValorarcomentarComponent
+    ValorarcomentarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
