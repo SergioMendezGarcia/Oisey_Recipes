220cfae9e0ddd38415e75ab2dde16173e3c0f236
JosePedro <alejandro.manzanares.01@gmail.com>
2019-04-23 09:18:42 +0200

    Parche Guille

--- OiseyApp/src/app/services/servicio-chef-comments.service.ts
+++ OiseyApp/src/app/services/servicio-chef-comments.service.ts
@@ -1,39 +1,61 @@
 import { Injectable } from '@angular/core';
 import { ComentarioChef } from '../Modelos/comentario-chef';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioChefCommentsService {
 
-  private _allChefComments = [
-    new ComentarioChef(
-      0,
-      'The best meal ever',
-      'Paquita Pérez',
-      'paquitaperez.jpg',
-      5,
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium ipsam dolore, totam suscipit nam commodi eos aut officia id, iusto accusamus quos ab ullam labore, debitis et pariatur quidem!',
-      '1 julio 2018'
-    ),
-    new ComentarioChef(
-      1,
-      'The worst meal ever',
-      'Francisco Soler',
-      'fsoler.jpg',
-      1,
-      'Lorem ipsum dolor, dolor el que se siente tras comer  sit amet consectetur adipisicing elit. Cumque praesentium ipsam dolore, totam suscipit nam commodi eos aut officia id, iusto accusamus quos ab ullam labore, debitis et pariatur quidem!',
-      '1 julio 2018'
-    ),
+  private _allChefComments = null;
+  private $commentsObserver = new BehaviorSubject(this._allChefComments);
+  private _commentObservable;
 
-  ];
+  // [
+  //   new ComentarioChef(
+  //     0,
+  //     'The best meal ever',
+  //     'Paquita Pérez',
+  //     'paquitaperez.jpg',
+  //     5,
+  //     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium ipsam dolore, totam suscipit nam commodi eos aut officia id, iusto accusamus quos ab ullam labore, debitis et pariatur quidem!',
+  //     '1 julio 2018'
+  //   ),
+  //   new ComentarioChef(
+  //     1,
+  //     'The worst meal ever',
+  //     'Francisco Soler',
+  //     'fsoler.jpg',
+  //     1,
+  //     'Lorem ipsum dolor, dolor el que se siente tras comer  sit amet consectetur adipisicing elit. Cumque praesentium ipsam dolore, totam suscipit nam commodi eos aut officia id, iusto accusamus quos ab ullam labore, debitis et pariatur quidem!',
+  //     '1 julio 2018'
+  //   ),
 
-  constructor() { };
+  // ];
+
+  constructor(private _http: HttpClient) { };
+
+
+  getCommentsFromAPI(): Observable<ComentarioChef[]> {
+    if (this._allChefComments) {
+      return of(this._allChefComments);
+    } else {
+      return this._http.get<ComentarioChef[]>('http://localhost:8080/api/pets').pipe(
+        tap(data => {
+          this._allChefComments = data;
+        })
+      );
+    }
+
+  }
                                     //Método para recuperar los comentarios filtrando por id
                                     //del comentario.
                                     //El método recibe como párametro el array con la lista
                                     //completa de comentarios del chef.
-  getComments(comentariosChefs) {
+  getComments() {
                                     //Declaración variable en la que se almacenarán los comentarios
                                     //ya filtrados.
 
--- OiseyApp/src/app/services/servicio-recetas.service.ts
+++ OiseyApp/src/app/services/servicio-recetas.service.ts
@@ -1,27 +1,52 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Receta } from '../Modelos/receta';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioRecetasService {
                                       //Array con datos mock para testeo
-  private  _recetas = [
-    new Receta(1, 'Huevos con patatas', 'Huevos, aceite, patatas', 25, ['https://bit.ly/2P6IIAu', 'patatas.jpg'], 5),
-    new Receta(2, 'Fabada asturiana', 'Fabes, Compango, Agua, Sal', 65, ['https://bit.ly/2v1he6f'], 5),
-    new Receta(3, 'Kebab de Burgos', 'Carne falsa, lechuga, salsa rara, cebolla', 15, ['https://bit.ly/2G6RI4t', 'kebab.jpg'], 2),
-    new Receta(4, 'Arroz tres delicias', 'Huevos, aceite de mala calidad, lechuga, zanahoria, dicen que es jamón cocido', 10, ['https://bit.ly/2UdUIB9', 'alozchino.jpg'], 2.5),
-    new Receta(5, 'Huevos con patatas', 'Huevos, aceite, patatas', 25, ['https://bit.ly/2P6IIAu', 'patatas.jpg'], 5),
-    new Receta(6, 'Fabada asturiana', 'Fabes, Compango, Agua, Sal', 65, ['https://bit.ly/2v1he6f'], 5),
-    new Receta(7, 'Kebab de Burgos', 'Carne falsa, lechuga, salsa rara, cebolla', 15, ['https://bit.ly/2G6RI4t', 'kebab.jpg'], 2),
-    new Receta(8, 'Arroz tres delicias', 'Huevos, aceite de mala calidad, lechuga, zanahoria, dicen que es jamón cocido', 10, ['https://bit.ly/2UdUIB9', 'alozchino.jpg'], 2.5)
-    ];
-                                    //Método para devolver el array mock _recetas
-  getRecetas(){return this._recetas};
+  private  _recetas = null;
 
-                                    //Método para filtar recetas
+  
+  //  [ new Receta(1, 'Huevos con patatas', 'Huevos, aceite, patatas', 25, ['https://bit.ly/2P6IIAu', 'patatas.jpg'], 5),
+  //   new Receta(2, 'Fabada asturiana', 'Fabes, Compango, Agua, Sal', 65, ['https://bit.ly/2v1he6f'], 5),
+  //   new Receta(3, 'Kebab de Burgos', 'Carne falsa, lechuga, salsa rara, cebolla', 15, ['https://bit.ly/2G6RI4t', 'kebab.jpg'], 2),
+  //   new Receta(4, 'Arroz tres delicias', 'Huevos, aceite de mala calidad, lechuga, zanahoria, dicen que es jamón cocido', 10, ['https://bit.ly/2UdUIB9', 'alozchino.jpg'], 2.5),
+  //   new Receta(5, 'Huevos con patatas', 'Huevos, aceite, patatas', 25, ['https://bit.ly/2P6IIAu', 'patatas.jpg'], 5),
+  //   new Receta(6, 'Fabada asturiana', 'Fabes, Compango, Agua, Sal', 65, ['https://bit.ly/2v1he6f'], 5),
+  //   new Receta(7, 'Kebab de Burgos', 'Carne falsa, lechuga, salsa rara, cebolla', 15, ['https://bit.ly/2G6RI4t', 'kebab.jpg'], 2),
+  //   new Receta(8, 'Arroz tres delicias', 'Huevos, aceite de mala calidad, lechuga, zanahoria, dicen que es jamón cocido', 10, ['https://bit.ly/2UdUIB9', 'alozchino.jpg'], 2.5)
+  //   ];
+
+  
+
+                                   
   
   // filterRecetas(nombre){ };
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+
+
+  getRecetasFromApi(): Observable<Receta[]> {
+    if (this._recetas) {
+      return of(this._recetas);
+    } else {
+      return this._http.get<Receta[]>('http://localhost:8080/api/pets').pipe(
+        tap(data => {
+          this._recetas = data;
+        })
+      );
+    }
+  };
+                                      //Método para devolver el array mock _receta
+                                      s
+  getRecetas(){return this._recetas};
+                                      //Método para filtar recetas
+
+
 }
