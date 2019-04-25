c4842bb7c5d860ece949956d613f44dbd2bbc83c
SergioMendezGarcia <a8alumno@aules.local>
2019-04-25 09:14:30 +0200

    parametros del id de chef

--- OiseyApp/src/app/pantalla-chef/pantalla-chef.component.ts
+++ OiseyApp/src/app/pantalla-chef/pantalla-chef.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { serviciochefs } from '../services/serviciochefs.service';
 import { ActivatedRoute } from '@angular/router';
 import { Chef } from '../Modelos/modeloChefs';
+import { ɵangular_packages_platform_browser_platform_browser_a } from '@angular/platform-browser';
 
 
 @Component({
@@ -23,7 +24,7 @@ export class PantallaChefComponent implements OnInit {
     // this.listaComments = this._chefCommServ.getComments(this.listaChefs[this.chefId].comentarios);
 
     this._route.params.subscribe(parametros => {
-      
+      parametros.chefId = "5cbf2170b81f9a0cb8fb310d";
 
         this.listaChefs = this._chefServ.getChefsFromAPI(parametros.chefId).subscribe(data=>{
         this.theChef = data;
