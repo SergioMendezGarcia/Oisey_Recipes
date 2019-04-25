35c0a1e1e89bcecd3240d831b343aa979b3d6177
SergioMendezGarcia <a8alumno@aules.local>
2019-04-25 11:55:25 +0200

    sdaSD

--- Server_oisey/app/router/chefs.js
+++ Server_oisey/app/router/chefs.js
@@ -53,51 +53,54 @@ router.route('/chefs')
 
     });
 
-    router.route('/chefs/:cid')
-        .get(function(req, res){
-            //res.json({data: 'hola:' + req.params.pid});
+router.route('/chefs/:cid')
+    .get(function (req, res) {
+        //res.json({data: 'hola:' + req.params.pid});
 
-            console.log('cid:',req.params.cid)
-            Chef.findById(req.params.cid).then(aChef=>{
-                res.json(aChef);
-            }).catch(err => {
-                console.log('error:', err);
-                res.status(500).send({ message: 'Server error'});
+        console.log('cid:', req.params.cid)
+        Chef.findById(req.params.cid).then(aChef => {
+            res.json(aChef);
+        }).catch(err => {
+            console.log('error:', err);
+            res.status(500).send({ message: 'Server error' });
+        });
+    })
+    .put(function (req, res) {
+        console.log('reqbody:', req.body);
+        if (!req.body) {
+            return res.status(400).send({
+                message: "Comment content can not be empty"
             });
-        })
-        .put(function(req, res) {
-            console.log('reqbody:', req.body);
-            if(!req.body) {
-                return res.status(400).send({
-                    message: "Comment content can not be empty"
-                });
+        }
+        Chef.findById(req.params.cid).then(unChef => {
+            if (unChef) {
+                console.log('actualizando unChef:', unChef);
+
+                unChef.comentarios.push({ commentText: req.body.comentario });
+                unChef.estrellas.push(req.body.estrellas);
+                unChef.markModified('comentarios');
+                unChef.markModified('estrellas');
             }
-            Chef.findById(req.params.cid).then(unChef=>{
-                if(unChef){
-                    unChef.comentarios.push({commentText:req.body.comentario});
-                    unChef.estrellas.push(req.body.estrellas);
-                    unChef.markModified('comentarios');
-                    unChef.markModified('estrellas');
-                }
-                return unChef
-            }).then(unChef=>{
-                if(unChef) {                    
-                    unChef.save();
-                }
 
-                console.log('unChef saved:', unChef);
+            return unChef
+        }).then(unChef => {
+            if (unChef) {
+                unChef.save();
+            }
 
-                return unChef
-            }).then(savedChef=>{
-                if (savedChef) {
-                    res.json(savedChef);
-                }
-            }).catch(err => {
-                console.log('Error saving new comment:', err);
-                res.status(500).send({ message: 'Server error' });
-            });
-           
+            console.log('unChef saved:', unChef);
+
+            return unChef
+        }).then(savedChef => {
+            if (savedChef) {
+                res.json(savedChef);
+            }
+        }).catch(err => {
+            console.log('Error saving new comment:', err);
+            res.status(500).send({ message: 'Server error' });
+        });
+
     });
-            
+
 
 module.exports = router;
